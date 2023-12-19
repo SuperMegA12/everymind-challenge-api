e3fb3c20e6b02272ac0f2bcfe04d686ac7b5080f
SuperMEgA12 <francisquinholeite@gmail.com>
2023-12-19 16:27:04 -0300

    first configurations all set

---
+++ src/main/java/nunes/api/nunessportsapi/NunesSportsApiApplication.java
@@ -0,0 +1,13 @@
+package nunes.api.nunessportsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NunesSportsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NunesSportsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nunes/api/nunessportsapi/NunesSportsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package nunes.api.nunessportsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NunesSportsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

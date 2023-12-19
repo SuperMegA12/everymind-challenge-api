7a3b05ca21757ac8f51d7ad5a256fec97f94f867
SuperMEgA12 <francisquinholeite@gmail.com>
2023-12-19 16:58:41 -0300

    product model created

---
+++ src/main/java/nunes/api/nunessportsapi/models/Product.java
@@ -0,0 +1,38 @@
+package nunes.api.nunessportsapi.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "PRODUCTS")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Product {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @NotNull
+  private String name;
+
+  @NotNull
+  private Long code;
+
+  @NotNull
+  private String description;
+
+  @NotNull
+  private Double price;
+}

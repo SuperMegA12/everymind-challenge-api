6a747e5c9016aaa1b355475108e874e26f6a8db5
SuperMEgA12 <francisquinholeite@gmail.com>
2023-12-19 17:11:36 -0300

    product repository

---
+++ src/main/java/nunes/api/nunessportsapi/repositories/product/ProductRepository.java
@@ -0,0 +1,11 @@
+package nunes.api.nunessportsapi.repositories.product;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import nunes.api.nunessportsapi.models.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+}
